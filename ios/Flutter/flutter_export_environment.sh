e21c9cb739940068503b38dc65fd76ff8f459a57
Le Dan <danle.sdev@gmail.com>
2019-12-08 23:54:49 +0700

    Update gitignore

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Tools/flutter"
-export "FLUTTER_APPLICATION_PATH=/Work/Personal/Project/mm_mobile_app/flutter_architecture_sample"
-export "FLUTTER_TARGET=/Work/Personal/Project/mm_mobile_app/flutter_architecture_sample/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Work/Personal/Project/flutter_architechture_sample"
+export "FLUTTER_TARGET=/Work/Personal/Project/flutter_architechture_sample/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Tools/flutter/bin/cache/artifacts/engine/ios"
